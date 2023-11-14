6cadb97e556794656a64d687ebee276bfeb8c750
KuyaGit <marklowelmontealto14@gmail.com>
2023-11-14 18:05:56 +0800

    deploy

---
+++ home.php
@@ -0,0 +1,267 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">s
+
+        <!--=============== REMIX ICONS ===============-->
+        <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+
+        <!--=============== CSS ===============-->
+        <link rel="stylesheet" href="assets/css/styles.css">
+
+        <title>SFA</title>
+    </head>
+    <body>
+        <!--==================== HEADER ====================-->
+        <header class="header" id="header">
+            <nav class="nav container">
+                <a href="#" class="nav__logo">
+                    <i class="img"><img style="width: 50px; height: 50px;" src="assets\img\sfalogo.png" alt=""></i> Saint Francis Academy
+                </a>
+
+                <div class="nav__menu" id="nav-menu">
+                    <ul class="nav__list">
+                        <li class="nav__item">
+                            <a href="#home" class="nav__link active-link">Home</a>
+                        </li>
+                        <li class="nav__item">
+                            <a href="#about" class="nav__link">About</a>
+                        </li>
+                       
+            
+                        <li class="nav__item">
+                            <a href="#contact" class="nav__link">Contact Us</a>
+                        </li>
+                        <li class="nav__item">
+                        <a href="index.php?q=enrol" class="nav_item" style="color: white;">Student</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+
+        <main class="main">
+            <!--==================== HOME ====================-->
+            <section class="home" id="home">
+                <div class="home__container container grid">
+                    <img src="assets/img/sfalogo.png" alt="" class="home__img">
+
+                    <div class="home__data">
+                        <h1 class="home__title">
+                           Saint Francis Academy
+                        </h1>
+
+                        <a href="index.php?q=enrol" class="button button--flex">
+                            Student Portal <i class="ri-arrow-right-down-line button__icon"></i>
+                        </a>
+                    </div>
+
+                    <div class="home__social">
+                        <span class="home__social-follow">Follow Us</span>
+
+                        <div class="home__social-links">
+                            <a href="https://www.facebook.com/" target="_blank" class="home__social-link">
+                                <i class="ri-facebook-fill"></i>
+                            </a>
+                           
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <!--==================== ABOUT ====================-->
+            <section class="about section container" id="about">
+                <div class="about__container grid">
+                    <img src="assets/img/sfa.png" alt="" class="about__img">
+
+                    <div class="about__data">
+                        <h2 class="section__title about__title">
+                          Welcome To <br> Saint Francis Academy
+                        </h2>
+
+                        <p class="about__description">
+                        Education: Schools provide students with the knowledge and 
+                        skills needed to succeed in life.
+
+                        </p>
+
+                        <div class="about__details">
+                            <p class="about__details-description">
+                                <i class="ri-checkbox-fill about__details-icon"></i>
+                            
+                                Personal Growth: Schools foster personal development, helping students build self-confidence, critical thinking abilities,<br> problem-solving skills, and a sense of responsibility.
+                            </p>
+                            <p class="about__details-description">
+                                <i class="ri-checkbox-fill about__details-icon"></i>
+                                Social Skills: Schools offer a social environment where students interact, learn to collaborate, and develop important social skills. They also promote diversity and tolerance, preparing students for a multicultural world.
+                            </p>
+                            
+                        </div>
+
+                        <a href="index.php?q=enrol" class="button--link button--flex">
+                            Login <i class="ri-arrow-right-down-line button__icon"></i>
+                        </a>
+                    </div>
+                </div>
+            </section>
+
+            <!--==================== STEPS ====================-->
+            <section class="steps section container">
+                <div class="steps__bg">
+                    <h2 class="section__title-center steps__title">
+                    MISSION & VISSION
+                    </h2>
+
+                    <div class="steps__container grid">
+                        <div class="steps__card">
+                            <div class="steps__card-number">Vission</div>
+                            
+                            <p class="steps__card-description">
+                                Saint Francis Academy is highly regarded for its academic
+                                excellence in educating young Christians who take active roles as
+                                citizens in our society. "Service for Academic Excellence in the Christian Society"
+
+                            </p>
+                        </div>
+
+                        <div class="steps__card">
+                            <div class="steps__card-number">Mission</div>
+                          
+                            <p class="steps__card-description">
+                                Saint Francis Academy advances the total human 
+                                development of learners socially, emotionally, 
+                                spiritually and academically, in order to enable 
+                                them to meet the challenges of a globally competitive 
+                                society. Further, in the service of God and country, 
+                                the institution strives to develop highly motivated 
+                                and committed teachers working in collaboration with 
+                                all stakeholders of the community, to develop learners 
+                                equipped with 21st century skills.
+                            </p>
+                        </div>    
+                    </div>
+                </div>
+            </section>
+
+            <!--==================== PRODUCTS ====================-->
+            <section class="product section container" id="products">
+                <h2 class="section__title-center">
+                    Core Values
+                </h2>
+
+                <p class="product__description">
+                    Francisian Core Values<br>
+<br>
+                    The principles emphatically interwoven in different discapinia 
+                    will bring forth our dreams for SFA students to a reality shaped 
+                    from our vision/mission, which showed the road map towards 
+                    accomplishment.
+                    <br>
+                    <br>
+                    1. Time Management: Life Management
+                    <br>
+                    <br>
+                    Students are given the opportunity to address questions related 
+                    to time management in varying levels and degrees: day-in and day- 
+                    out, moment by moment. Enriching experiences in terms of purposes. 
+                    values roles and priorities. Those are done through activides 
+                    inside the classroom, in the campus and out campus activities joined 
+                    by them.
+                    <br>
+                    <br>
+                    2. Learning for All
+                    <br>
+                    <br>
+                    We accord high regard to students whose talents scored nigh above average 
+                    and average But what do we do to those who hardly make meaning of and 
+                    hardly understand the lesson? We keep them informed that human capacity 
+                    to learn is unlimited and we tend to develop the climate of high expoctancy 
+                    for bethachers and students no matter what his/her age We are firm in our 
+                    bolist that what we expect happens and had been proven in many previous 
+                    batches of students.
+                </p>
+
+            </section>
+
+            <!--==================== QUESTIONS ====================-->
+            <section class="questions section" id="faqs" >
+                <section class="contact section container" id="contact" style="margin-left: 250px;">                
+                    <div class="contact__container grid">
+                        <div class="contact__box">
+                        <footer class="footer section">
+            <div class="footer__container container grid">
+                <div class="footer__content">
+                    <a href="#" class="footer__logo">
+                        <i class="img"><img style="width: 70px; height: 70px;" src="assets/img/sfalogo.png" alt=""></i> Saint Francis Academy
+                    </a>
+
+                  
+
+                   
+
+                <div class="footer__content" >
+                    <h3 class="footer__title">Our Address</h3>
+
+                    <ul class="footer__data">
+                        <li class="footer__information" style="margin-left: -70px;">Poblacion, Mabini, Batangas </li>
+        
+
+                    </ul>
+                </div>
+
+                <div class="footer__content">
+                    <h3 class="footer__title">Contact Us</h3>
+
+                    <ul class="footer__data">
+                        <li class="footer__information" style="margin-left: -15px;">0905298862</li>
+                        <li class="footer__information" style="margin-left: -15px;">09177205340</li>
+                        
+                        <div class="footer__social">
+                            <a href="https://www.facebook.com/sfamabini1967?mibextid=ZbWKwL" class="footer__social-link">
+                                <i style="margin-left: 10px;" class="fa fa-facebook"></i>
+                            </a>
+                            <a href="sfamb1967@yahoo.com.ph" class="footer__social-link">
+                                <i class="fa fa-envelope"></i>
+                            </a>
+                            
+                            </a>
+                        </div>
+                    </ul>
+                </div>
+
+                <div class="footer__content">
+                    <h3 class="footer__title">
+                  
+                    </h3>
+
+                    <div style="margin-left: 24px;" class="footer__cards">
+                    ©SAINT FRANCIS ACADEMY. All rigths reserved
+                    </div>
+                </div>
+            </div>
+
+            
+        </footer>
+                        
+                    </div>
+                </section>
+            </section>
+
+         
+
+        <!--==================== FOOTER ====================-->
+       
+        
+        <!--=============== SCROLL UP ===============-->
+        <a href="#" class="scrollup" id="scroll-up"> 
+            <i class="ri-arrow-up-fill scrollup__icon"></i>
+        </a>
+
+        <!--=============== SCROLL REVEAL ===============-->
+        <script src="assets/js/scrollreveal.min.js"></script>
+        
+        <!--=============== MAIN JS ===============-->
+        <script src="assets/js/main.js"></script>
+    </body>
+</html>
